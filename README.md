9292f9fbc50bf773e5581a355ec70c4d2a87312a
E <113189521+EmmaBejan@users.noreply.github.com>
2023-01-23 15:41:43 +0000

    .

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# ICS2OFinalProject
-Final Project for ICS2O
-
-This is the repo that you will use for your final project.
+# FinalProject
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+print("Hello and welcome to my rock, paper and scissors game!")
+
 options = ("rock", "paper", "scissor")
 running = True
 
@@ -9,21 +11,22 @@ while running:
   computer = random.choice(options)
 
   while player not in options:
-    player = input("Please choose (rock, paper, scissors): ")
+    player = input("Please choose between (rock, paper, scissor): ")
 
   print(f"Player: {player}")
   print(f"Computer: {computer}")
 
   if player == computer:
     print("It's a tie!")
-  elif player == "scissors" and computer == "paper":
+  elif player == "scissor" and computer == "paper":
     print("You win!")
   elif player == "paper" and computer == "rock":
     print("You win!")
-  elif player == "rock" and computer == "scissors":
-    print("You win!")
+  elif player == "rock" and computer == "scissor":
+    print("You win! Good job")
   else:
     print("Computer wins! You Lose, haha!")
   if not input("Want to play again? (y/n): ").lower() == "y":
     break
 print("Thank you for playing :)")
+
